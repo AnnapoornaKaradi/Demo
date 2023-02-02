7e6a7a82b7d1e6e4360677c5dd7856e3e586c866
AnnapoornaKaradi <122218758+AnnapoornaKaradi@users.noreply.github.com>
2023-02-02 16:48:21 +0530

    code updated

--- src/test/java/Selenium_Demo/SeleniumMavenDemo/AppTest.java
+++ src/test/java/Selenium_Demo/SeleniumMavenDemo/AppTest.java
@@ -10,6 +10,14 @@ public class AppTest {
   public void f() {
 	  System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		WebDriver driver=new ChromeDriver();
+	  ChromeOptions options = new ChromeOptions();
+	options.addArguments("start-maximized"); // open Browser in maximized mode
+	options.addArguments("disable-infobars"); // disabling infobars
+	options.addArguments("--disable-extensions"); // disabling extensions
+	options.addArguments("--disable-gpu"); // applicable to windows os only
+	options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+	options.addArguments("--no-sandbox"); // Bypass OS security model
+	WebDriver driver = new ChromeDriver(options);
 		driver.navigate().to("https://www.amazon.in/");
 		driver.findElement(By.name("field-keywords")).sendKeys("dresses");
 		driver.findElement(By.id("nav-search-submit-button")).click();
